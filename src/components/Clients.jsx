3df39fd22285659554b28f662c7759f24750d1e6
Vikas Prajapati <2023ume0280@iitjammu.ac.in>
2025-05-28 17:13:15 +0530

    done for now

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { clients } from "../constants";
+import styles from "../style";
 
 const Clients = () => {
   return (
-    <div>
-      
-    </div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className="logo-slider w-full">
+        <div className="logo-track">
+          {/* Triple the logos for ultra-smooth infinite scroll */}
+          {[...Array(3)].map((_, setIndex) => 
+            clients.map((c, index) => (
+              <div key={`${setIndex}-${index}`} className="logo-item">
+                <img 
+                  src={c.logo} 
+                  alt='logo'
+                  className="sm:w-[192px] w-[100px] object-contain"
+                />
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/style.js
+++ src/style.js
@@ -25,4 +25,5 @@ export const layout = {
   sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
 };
 
+
 export default styles;
